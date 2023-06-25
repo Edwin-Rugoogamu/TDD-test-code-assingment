6156cc7a4daf997a07e02dea26865f2b168e5a9c
Edwin Rugoogamu <rugoogamuedwin5@gmail.com>
2023-06-25 13:32:35 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+npm install chai mocha --save-dev 
+ npm install --global mocha 
+ npm init @eslint/config
+ npm i eslint-config-prettier
+npx eslint --init (gives several options)
+npx install-peerdeps --dev eslint-config-airbnb
---
+++ test/test.js
@@ -0,0 +1,82 @@
+var assert = require('assert');
+ require("./multiply")
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return 1 when the value is multiplied',  function(){
+
+         let  multiply= (a,b)=>{
+            return(a*b)
+           
+         }
+         assert.equal(multiply(1,1), 1)
+      });
+    });
+  });
+
+  describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return 4 when the value is multiplied',  function(){
+
+         let  multiply= (a,b)=>{
+            return(a*b)
+           
+         }
+         assert.equal(multiply(2, 2),4)
+      });
+    });
+  });
+
+
+  describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return 4 when the value is multiplied',  function(){
+
+         let  multiply= (a,b)=>{
+            return(a*b)
+           
+         }
+         assert.equal(multiply(3, 3), 9)
+      });
+    });
+  });
+
+
+  describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return 4 when the value is multiplied',  function(){
+
+         let  multiply= (a,b)=>{
+            return(a*b)
+           
+         }
+         assert.equal(multiply(4, 4), 16)
+      });
+    });
+  });
+
+  describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return 4 when the value is multiplied',  function(){
+
+         let  multiply= (a,b)=>{
+            return(a*b)
+           
+         }
+         assert.equal(multiply(23, 45), 23 * 45)
+      });
+    });
+  });
+
+
+
+
+  
